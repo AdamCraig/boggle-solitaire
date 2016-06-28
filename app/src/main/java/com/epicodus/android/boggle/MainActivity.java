eceb05b473a70a06ec0e42639a7cbe66b7b95d30
Adam Craig and Andrew Russo <adam+andrew@>
2016-06-28 11:18:28 -0700

    Successfully have a way to check words that are entered and determine if they meet the prerequisites of the game, and then have them stored to be displayed in the next activity.

--- app/src/main/java/com/epicodus/android/boggle/MainActivity.java
+++ app/src/main/java/com/epicodus/android/boggle/MainActivity.java
@@ -2,6 +2,9 @@ package com.epicodus.android.boggle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,10 +18,13 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.randomStringTextView) TextView mRandomStringTextView;
+    @Bind(R.id.submitButton) Button mSubmitButton;
+    @Bind(R.id.userEditText) EditText mUserEditText;
 
     public char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
     public char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
     public ArrayList<Character> randomStringArray = new ArrayList<Character>();
+    public ArrayList<String> userWords = new ArrayList<String>();
     public String randomString = "";
 
     @Override
@@ -28,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Random randomGenerator = new Random();
 
-        for(int index = 0; index < 2; index++) {
+        for (int index = 0; index < 2; index++) {
             int randomInt = randomGenerator.nextInt(4);
             randomStringArray.add(vowels[randomInt]);
         }
@@ -44,6 +50,38 @@ public class MainActivity extends AppCompatActivity {
             randomString += character;
         }
 
-       mRandomStringTextView.setText(randomString);
+        randomString = randomString.toUpperCase();
+
+        mRandomStringTextView.setText(randomString);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userWord = mUserEditText.getText().toString().toUpperCase();
+                int letterCounter = 0;
+
+                if (userWord.length() >= 3 && userWord.length() <= 8) {
+                    for (int userIndex = 0; userIndex < userWord.length(); userIndex++) {
+                        for (int boggleIndex = 0; boggleIndex < randomString.length(); boggleIndex++) {
+                            if (userWord.charAt(userIndex) == (randomString.charAt(boggleIndex))) {
+                                letterCounter++;
+                                break;
+                            }
+                        }
+                    }
+                    if (letterCounter >= 3) {
+                        userWords.add(userWord);
+                        Toast.makeText(MainActivity.this, "User words: "+userWords.toStriWng(), Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Unacceptable!", Toast.LENGTH_LONG).show();
+                    }
+
+                } else {
+                   Toast.makeText(MainActivity.this, "Invalid entry length", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+
 }
