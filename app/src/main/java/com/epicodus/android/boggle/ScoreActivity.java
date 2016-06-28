0ca9dd461470f1f68280d358f4013e9ecfdd78be
Adam Craig and Andrew Russo <andrew@>
2016-06-28 15:18:55 -0700

    Have words displaying on ScoreActivity page, displaying the words submitted and the score of the player.

--- app/src/main/java/com/epicodus/android/boggle/ScoreActivity.java
+++ app/src/main/java/com/epicodus/android/boggle/ScoreActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -12,6 +15,8 @@ import butterknife.ButterKnife;
 
 public class ScoreActivity extends AppCompatActivity {
 
+    @Bind(R.id.scoreListView) ListView mScoreListView;
+    @Bind(R.id.scoreTitleTextView) TextView mScoreTitleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,12 @@ public class ScoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_score);
         Intent intent = getIntent();
         ArrayList<String> userWords = intent.getStringArrayListExtra("userWords");
-        Log.d("userWords", "userWords:" + userWords);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, userWords);
+        mScoreListView.setAdapter(adapter);
+        String userScore = Integer.toString(userWords.size());
+        mScoreTitleTextView.setText("Your Score: " + userScore);
 
     }
 }
